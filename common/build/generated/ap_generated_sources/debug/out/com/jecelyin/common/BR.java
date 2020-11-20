bb07992883f6b77483ab124e6c5dbbe007c816ab
Cregrant <cregrant90@gmail.com>
2020-11-20 15:51:10 +0700

    trash removed

--- common/build/generated/ap_generated_sources/debug/out/com/jecelyin/common/BR.java
+++
@@ -1,5 +0,0 @@
-package com.jecelyin.common;
-
-public class BR {
-  public static int _all = 0;
-}
--- common/build/generated/source/dataBinding/trigger/debug/com/jecelyin/common/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package com.jecelyin.common;
-
-import androidx.databinding.BindingBuildInfo;
-
-@BindingBuildInfo
-public class DataBindingInfo {}
