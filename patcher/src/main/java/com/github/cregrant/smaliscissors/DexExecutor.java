1a4c8cc0b56f8f5d03f380da004fde2acc8ba7d3
Cregrant <cregrant90@gmail.com>
2023-07-21 21:47:17 +0700

    refactor: implement patcher through jitpack instead of a local folder

--- patcher/src/main/java/com/github/cregrant/smaliscissors/DexExecutor.java
+++
@@ -1,5 +0,0 @@
-package com.github.cregrant.smaliscissors;
-
-public interface DexExecutor {
-    void runDex(String dexPath, String entrance, String mainClass, String apkPath, String zipPath, String projectPath, String param, String tempDir);
-}
